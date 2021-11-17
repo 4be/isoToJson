23f94fffe2f9813a629cf1f1f48f32835ae7d077
adiabdurakhim <jaderizan@gmail.com>
2021-11-17 09:34:07 +0700

    first up

---
+++ src/main/java/com/simas/rdn/ksei/KseiValidateAckApplication.java
@@ -0,0 +1,13 @@
+package com.simas.rdn.ksei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KseiValidateAckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KseiValidateAckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simas/rdn/ksei/KseiValidateAckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simas.rdn.ksei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KseiValidateAckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
